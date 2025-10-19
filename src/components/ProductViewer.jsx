77118e9ec71223608d6c02c26b00d21bb838ed96
Niki Tsolaki <nikitsolaki10@gmail.com>
2025-10-19 22:56:45 +0300

    Model rendering, lights

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,8 @@ import useMacBookStore from "../store";
 import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
+import MacBookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./StudioLights.jsx";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacBookStore();
@@ -40,7 +42,8 @@ const ProductViewer = () => {
 
             <p className="text-white text-4xl">Render Canvas</p>
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}} >
-                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+                <StudioLights />
+                <MacBookModel14 scale={0.06} position={[0, 0, 0]} />
                 <OrbitControls enableZoom={false} />
             </Canvas>
         </section>
